7d29b2163f695f343abcdb92dbe5937d3b12ccc4
PM04Hugo <PM04Hugo@hotmail.com>
2026-04-03 10:24:58 +0200

    Reglas done

--- Estandar/models.py
+++ Estandar/models.py
@@ -17,7 +17,17 @@ class Medidas(models.Model):
     nombre = models.CharField(max_length=20)
     unidades = models.ManyToManyField(Unidades)
 
+
     def __str__(self):
         return self.nombre
     
 
+class Regla(models.Model):
+    nombre = models.CharField(max_length=20)
+    nombre_medida = models.CharField(max_length=20)
+    nombre_unidad = models.CharField(max_length=20)
+    descripcion = models.TextField()
+    minimo = models.FloatField()
+    maximo = models.FloatField()
+    
+    
