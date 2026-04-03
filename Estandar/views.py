1229666a5cfd90a0443668953618575f25d8640d
PM04Hugo <PM04Hugo@hotmail.com>
2026-04-03 10:24:58 +0200

    Reglas done

--- Estandar/views.py
+++ Estandar/views.py
@@ -3,10 +3,22 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 import json
-from .models import Medidas
+from .models import Medidas, Regla
 
 
 def formulario(request):
+    
+    if request.method == 'POST':
+        Regla.objects.create(
+            nombre=request.POST.get('regla'),
+            nombre_medida=request.POST.get('nombre'),
+            nombre_unidad=request.POST.get('unidades'),
+            descripcion=request.POST.get('descripcion'),
+            minimo=float(request.POST.get('minimo')),
+            maximo=float(request.POST.get('maximo')),
+        )
+        return redirect('base')
+    
     medidas = Medidas.objects.prefetch_related('unidades').all()
     return render(request, 'formulario.html', {'medidas': medidas})
 def form(request):
